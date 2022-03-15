726510fd218017be6f74138baa80a1f08e7ba16d
Bruno Cunha <bruno.silva.cunha@hotmail.com>
2022-03-14 21:46:58 -0300

    Popular home team and away team da partd

--- src/repositories/FixtureRepository.js
+++ src/repositories/FixtureRepository.js
@@ -1,4 +1,9 @@
 import Fixture from '../models/Fixture';
+import Team from '../models/Team';
 import BaseRepository from './BaseRepository';
 
-export default BaseRepository(Fixture);
+export default {
+  ...BaseRepository(Fixture),
+  findAll: () => Fixture.findAll({ include: [{ model: Team, as: 'AwayTeam' }, { model: Team, as: 'HomeTeam' }] }),
+  getById: (id) => Fixture.findByPk(id, { include: [{ model: Team, as: 'AwayTeam' }, { model: Team, as: 'HomeTeam' }] }),
+};
